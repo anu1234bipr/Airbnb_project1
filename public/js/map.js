e9a0d72497ef76e047ed75161e2879ad32555bd7
anu1234bipr <anuragchakraborty974@gmail.com>
2025-08-09 10:49:49 +0530

    some style add

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,7 @@
   console.log(mapboxtoken);
     const map = new mapboxgl.Map({
         container: 'map', // container ID
+        style: "mapbox://styles/mapbox/dark-v11",
         center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
